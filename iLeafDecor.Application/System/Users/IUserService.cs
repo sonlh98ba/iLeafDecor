9e917c15e27930187bf9fa5a5ca606133e3c085d
sonlh98ba <sonlh98ba@gmail.com>
2021-09-03 21:28:36 +0700

    Update

---
+++ iLeafDecor.Application/System/Users/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using iLeafDecor.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iLeafDecor.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authencate(LoginRequest request);
+
+        Task<bool> Register(RegisterRequest request);
+    }
+}
